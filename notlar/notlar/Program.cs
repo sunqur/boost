ff6c0b9410d1c2a1b3858d6ff73dd2feee3bb6d1
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-15 13:57:51 +0300

    dışarıdan alınan 2 sayı ile not ortalaması hesaplanıp geçme notu verildi

---
+++ notlar/notlar/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoşgeldiniz...");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. sınavın notu");
+            int not1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. sınavın notu");
+            int not2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("------------------");
+            double ort = (not1 + not2) / 2;
+            if (ort>=0 && ort<45)
+            {
+                Console.WriteLine("Başarısız");
+                Console.WriteLine("******************");
+                Console.WriteLine("Ortalamanız : "+ort);
+
+            }
+            else if (ort >= 45 && ort < 55)
+            {
+                Console.WriteLine("geçer");
+                Console.WriteLine("******************");
+                Console.WriteLine("Ortalamanız : " + ort);
+
+            }
+            else if (ort >= 55 && ort < 70)
+            {
+                Console.WriteLine("Orta");
+                Console.WriteLine("******************");
+                Console.WriteLine("Ortalamanız : " + ort);
+            }
+            else if (ort >= 70 && ort < 85)
+            {
+                Console.WriteLine("iyi");
+                Console.WriteLine("******************");
+                Console.WriteLine("Ortalamanız : " + ort);
+            }
+            else if (ort >= 85 && ort < 100)
+            {
+                Console.WriteLine("Çok iyi");
+                Console.WriteLine("******************");
+                Console.WriteLine("Ortalamanız : " + ort);
+            }
+            else
+            {
+                Console.WriteLine("Yanlış değerler girildi!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
