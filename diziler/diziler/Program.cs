a60d3c5a8fd58456f7064cda5db189658b7aedd2
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-16 11:41:23 +0300

    20 elemanlı -100 ile + 100 arası rastgele sayı üretme ve poztif negatif sıfır sayılarının adedini bulma

--- diziler/diziler/Program.cs
+++ diziler/diziler/Program.cs
@@ -10,21 +10,51 @@ namespace diziler
     {
         static void Main(string[] args)
         {
-            int[] dizi = new int[500];
+            int[] dizi = new int[20];
             Random rnd = new Random();
-
-            for (int i = 0; i < dizi.Length; i++)
+            int nesay = 0;
+            int posay = 0;
+            int sifsay = 0;
+            for (int i = 0; i < 20; i++)
             {
-                int sayi = rnd.Next(1, 50);
+                int sayi = rnd.Next(-100, 100);
                 dizi[i] = sayi;
-                
+
+                if (dizi[i] < 0)
+                {
+
+                    nesay++;
+                    
+                    Console.WriteLine(dizi[i] + "\t");
+                }
+                else if (dizi[i] > 0)
+                {
+                    
+                    posay++;
+                   
+                    Console.WriteLine(dizi[i] + "\t");
+                }
+                else
+                {
+                    sifsay++;
+                   
+                    Console.WriteLine(dizi[i] + "\t");
+                }
             }
-            Array.Sort(dizi); // küçükten büyüğe
-            Array.Reverse(dizi);// büyükten küçüğe
-            for (int i = 0; i < 500; i++)
-            {
-                Console.Write(dizi[i] + "\t");
-            }
+           
+
+            Console.WriteLine("negatif sayıların adedi : " + nesay);
+            Console.WriteLine("0 sayıların adedi : " + sifsay);
+            Console.WriteLine("pozitif sayıların adedi : " + posay);
+            
+
+
+            //Array.Sort(dizi); // küçükten büyüğe
+            //Array.Reverse(dizi);// büyükten küçüğe
+            //for (int i = 0; i < 500; i++)
+            //{
+            //    Console.Write(dizi[i] + "\t");
+            //}
             Console.ReadLine();
 
         }
