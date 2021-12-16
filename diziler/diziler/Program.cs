c1073f7c2c63b96fae4b62a95ec669590773e8b8
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-16 10:11:34 +0300

    1 ile 50 arası rastgele 500 tane sayı üretip küçükten büyüğe ve büyükten küçüğe sıralama

---
+++ diziler/diziler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[500];
+            Random rnd = new Random();
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                int sayi = rnd.Next(1, 50);
+                dizi[i] = sayi;
+                
+            }
+            Array.Sort(dizi); // küçükten büyüğe
+            Array.Reverse(dizi);// büyükten küçüğe
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(dizi[i] + "\t");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
