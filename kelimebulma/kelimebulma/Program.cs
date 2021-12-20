c2fa7efb6d69d801bd573fc19a75b243f84721bb
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-20 15:19:00 +0300

    char ile alfabeyi sırayla diziye işleme

--- kelimebulma/kelimebulma/Program.cs
+++ kelimebulma/kelimebulma/Program.cs
@@ -10,30 +10,59 @@ namespace kelimebulma
     {
         static void Main(string[] args)
         {
-            int sonuc = 0;
-            string kelime = "";
-            Console.WriteLine("Cümleyi giriniz");
-            string cumle = Console.ReadLine();
-            Console.WriteLine("Aranacak olan kelimeyi giriniz");
-            string aranan = Console.ReadLine();
+            //int sonuc = 0;
+            //string kelime = "";
+            //Console.WriteLine("Cümleyi giriniz");
+            //string cumle = Console.ReadLine();
+            //Console.WriteLine("Aranacak olan kelimeyi giriniz");
+            //string aranan = Console.ReadLine();
 
-            for (int i = 0; i < cumle.Length; i++)
-            {
-                if (cumle[i]!=' ')
-                {
-                    kelime += cumle[i];
-                }
-                else
-                {
-                    if (kelime == aranan)
-                    {
-                        sonuc++;
-                    }
-                    kelime = "";
-                }
-            }
+            //for (int i = 0; i < cumle.Length; i++)
+            //{
+            //    if (cumle[i]!=' ')
+            //    {
+            //        kelime += cumle[i];
+            //    }
+            //    else
+            //    {
+            //        if (kelime == aranan)
+            //        {
+            //            sonuc++;
+            //        }
+            //        kelime = "";
+            //    }
+            //}
 
-            Console.WriteLine("Sonuc : {0} ", sonuc.ToString());
+            //Console.WriteLine("Sonuc : {0} ", sonuc.ToString());
+
+
+
+            //Random rnd = new Random();
+            //int[] dizi = new int[5];
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    dizi[i] = rnd.Next(100);
+            //}
+            //Array.Sort(dizi);
+            //Array.Reverse(dizi);
+
+            //foreach (var item in dizi)
+            //{
+            //    Console.WriteLine(item + "\t");
+            //}
+
+            //char[] alf = new char[26];
+            //int sira = 0;
+
+            //for (char i = 'a'; i <= 'z'; i++)
+            //{
+            //    alf[sira] = i;
+            //    Console.Write(alf[sira] +"\t");
+            //    sira++;
+            //}
+
+
+
 
             Console.ReadLine();
         }
