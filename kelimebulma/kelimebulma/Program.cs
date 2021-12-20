1f8a9875a90a3c8d87842526519ef91a60735f1d
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-20 14:25:35 +0300

    KELİME BULMA

---
+++ kelimebulma/kelimebulma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kelimebulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = 0;
+            string kelime = "";
+            Console.WriteLine("Cümleyi giriniz");
+            string cumle = Console.ReadLine();
+            Console.WriteLine("Aranacak olan kelimeyi giriniz");
+            string aranan = Console.ReadLine();
+
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i]!=' ')
+                {
+                    kelime += cumle[i];
+                }
+                else
+                {
+                    if (kelime == aranan)
+                    {
+                        sonuc++;
+                    }
+                    kelime = "";
+                }
+            }
+
+            Console.WriteLine("Sonuc : {0} ", sonuc.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
