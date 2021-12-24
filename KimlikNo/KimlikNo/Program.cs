127f7699aac0173001c24d3b778648fbe25b4397
Mustafa Sungur TAS <sunqurtas@gmail.com>
2021-12-24 16:42:44 +0300

    metot örnekleme

---
+++ KimlikNo/KimlikNo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KimlikNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(TcNoUret());
+
+            Console.ReadKey();
+        }
+
+        static string TcNoUret()
+        {
+            string tc = "";
+            Random rnd = new Random();
+            tc += rnd.Next(1, 10);
+
+            for (int i = 0; i < 8; i++)
+            {
+                tc += rnd.Next(10);
+            }
+
+            int tekToplam = Convert.ToInt32(tc[0].ToString()) +
+                Convert.ToInt32(tc[2].ToString()) + Convert.ToInt32(tc[4].ToString()) +
+                Convert.ToInt32(tc[6].ToString()) + Convert.ToInt32(tc[8].ToString());
+            int ciftToplam = Convert.ToInt32(tc[0].ToString()) +
+                Convert.ToInt32(tc[1].ToString()) + Convert.ToInt32(tc[3].ToString()) +
+                Convert.ToInt32(tc[5].ToString()) + Convert.ToInt32(tc[7].ToString());
+
+            int hane10 = (tekToplam * 7 - ciftToplam) % 10;
+            tc += hane10;
+
+            int ilk10HaneToplam = 0;
+            for (int i = 0; i < tc.Length; i++)
+            {
+                ilk10HaneToplam += Convert.ToInt32(tc[i].ToString());
+            }
+
+            tc += ilk10HaneToplam % 10;
+            return tc;
+        }
+    }
+}
---
+++ Ucgen/Ucgen/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region UcgenCagirma
+            //Console.WriteLine("Hoşgeldiniz... Oluşturmak istediğiniz üçgenin 1. Kenarını giriniz");
+            //double kenar1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Oluşturmak istediğiniz üçgenin 2. Kenarını giriniz");
+            //double kenar2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Oluşturmak istediğiniz üçgenin 3. Kenarını giriniz");
+            //double kenar3 = Convert.ToDouble(Console.ReadLine());
+
+            //Bul(kenar1,kenar2,kenar3);
+            #endregion
+            #region ParamsEnbuyuksayi
+            //int sonuc = EnBuyukSayiyiBul(10, 5, 7, 99, 1531, 22, 331313);
+            //Console.WriteLine("En büyük sayı : "+sonuc);
+
+            //Console.ReadKey();
+            #endregion
+
+
+            #region Fibonacci
+            /* 
+            Fibonacci Dizisi'ni oluşturmak için şu formül uygulanmaktadır: 
+            F(n) = F(n - 1) + F(n - 2). 
+            İşte bu formül Fibonacci Dizisi algoritmasıdır. 
+            
+            Console.WriteLine("değeri girin : ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\n" + fibonacci(i));
+            }
+
+            Console.ReadKey();
+            */
+            #endregion
+
+        }
+        #region UcgenBulmaMetot
+        //static void Bul(double kenar1, double kenar2, double kenar3)
+        //{
+        //    if ((kenar1 > 0 && kenar2 > 0 && kenar3 > 0) &&
+        //       Math.Abs(kenar3 - kenar2) < kenar1 && kenar1 < (kenar2 + kenar3) &&
+        //       Math.Abs(kenar1 - kenar3) < kenar2 && kenar2 < (kenar1 + kenar3) &&
+        //       Math.Abs(kenar1 - kenar2) < kenar3 && kenar3 < (kenar1 + kenar2)
+        //      )
+        //    {
+        //        if (kenar1 == kenar2 && kenar1 == kenar3 && kenar3 == kenar2)
+        //        {
+        //            Console.WriteLine("Üçgenimiz eşkenar üçgendir");
+
+        //        }
+        //        else if (
+        //            (kenar1 == kenar2 && kenar2 != kenar3) ||
+        //            (kenar1 == kenar3 && kenar2 != kenar3) ||
+        //            (kenar3 == kenar2 && kenar1 != kenar3))
+        //        {
+        //            Console.WriteLine("Üçgenimiz ikizkenar üçgendir");
+
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Üçgenimiz çeşitkenar üçgendir");
+
+        //        }
+        //    }
+
+        //    else
+        //        Console.WriteLine("verilen bilgilerle üçgenin çizilmesi mümkün değildir!!!");
+
+
+        //}
+
+        #endregion
+        #region ParamsEnbüyüksayıyıBulma
+        //static int EnBuyukSayiyiBul(params int[] sayilar)
+        //{
+        //    int enBuyukSayi = -1;
+
+        //    if (sayilar.Length > 0)
+        //    {
+        //        enBuyukSayi = sayilar[0];
+        //    }
+
+        //    foreach (int sayi in sayilar)
+        //    {
+        //        if (enBuyukSayi < sayi)
+        //        {
+        //            enBuyukSayi = sayi;
+        //        }
+        //    }
+        //    return enBuyukSayi;
+        //}
+        #endregion
+
+        #region FibonacciMetot
+
+        //static int fibonacci(int sayi)
+        //{
+        //    if (sayi == 0)
+        //    {
+        //        return 0;
+
+        //    }
+        //    else if (sayi == 1)
+        //    {
+        //        return 1;
+        //    }
+        //    else
+        //    {
+        //        return fibonacci(sayi - 1) + fibonacci(sayi - 2);
+        //    }
+        //}
+        #endregion
+
+    }
+}
